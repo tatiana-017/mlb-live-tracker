9aa704e05aab60550269ba51f358a5b09cde648a
Jeimy Arias <tr2303100@gmail.com>
2025-02-04 14:05:26 -0500

    configuración gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    ghPages()
   ],
+  base: "mlb-live-tracker",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
